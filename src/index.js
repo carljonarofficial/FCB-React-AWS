a30fb725c36210d46fb01c18ade0b5982647d150
Carl Jonar Palado <cjpalado@gmail.com>
2022-01-21 14:42:47 +0800

    Create separate JS File for Task and Taskslist

--- src/index.js
+++ src/index.js
@@ -1,31 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-
-function Task(props) {
-	return (
-		<li className="tasks-item">{props.taskName}</li>
-	);
-}
-
-function TasksList() {
-	const taskItemsList = [
-		"Follow Edukasyon.ph on Facebook.",
-		"Follow AWS Siklab Pilipinas on Facebook.", 
-		"Follow Zuitt Coding Bootcamp on Facebook."
-	];
-
-	return (
-		<div>
-			<input className="task-input" />
-			<ul>
-				{taskItemsList.map((task, index) => {
-					return <Task key={index} taskName={task} />;
-				})}
-			</ul>
-		</div>
-	);
-}
+import TasksList from "./components/TasksList";
 
 function App() {
 	const paraStyle = {
